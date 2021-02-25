1b265dd0d3857dcbed511a397b71da9e576f4a18
Nyitrai Orsolya <orsinyitrai2001@gmail.com>
2021-02-25 20:45:56 +0200

    Updated: Added the first 3 exercise

--- Homework2/src/BankAccount.java
+++ Homework2/src/BankAccount.java
@@ -1,2 +1,33 @@
 public class BankAccount {
+    private double balance;
+    private String accountNumber;
+
+    // shadowing
+    public BankAccount(String accountNumber){
+        // accountNumber - local variable (parameter)
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void deposit(double amount){
+        if(amount <= 0){
+            return;
+        }
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount){
+        if(amount > balance){
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
 }
