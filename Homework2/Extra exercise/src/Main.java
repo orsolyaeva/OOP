10c58705f2e407bb46c9651b75bce61255874daa
Nyitrai Orsolya <orsinyitrai2001@gmail.com>
2021-02-26 18:02:41 +0200

    Finished assignment

--- Homework2/Extra exercise/src/Main.java
+++ Homework2/Extra exercise/src/Main.java
@@ -3,68 +3,82 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-//	    Car car = new Car("Ford", "Fiesta", 2, true, 2002);
-	    boolean ok = false;
-        int countDiesel = 0, countRedCars = 0;
-//        System.out.println(car);
-        try{
-            File file = new File("input.txt");
-            Scanner myReader = new Scanner(file);
-            int nrCars = myReader.nextInt();
-            myReader.nextLine();
-            ArrayList<Car> cars = new ArrayList<>(nrCars);
-            Random rand = new Random();
-            for(int i = 0; i < nrCars; i++) {
-                int fuelType = rand.nextInt(3);
-                boolean passengers = rand.nextBoolean();
-                String carType = myReader.nextLine();
-                String carBrand = myReader.nextLine();
-                String yearManufacture = myReader.nextLine();
-                int year = Integer.parseInt(yearManufacture);
-
-                if(fuelType == 0){
-                    countDiesel += 1;
-                }
-
-                if(carType.charAt(0) == 'R'){
-                    countRedCars += 1;
-                }
-
-                cars.add(new Car(carType, carBrand, fuelType, passengers, year));
-
-                int increase;
-                Scanner read = new Scanner(System.in);
-                System.out.println("Enter how many times to increase the speed: ");
-                increase = read.nextInt();
-                for(int j = 0; j < increase; j++) {
-                    int kilometer = 1 + rand.nextInt(50);
-                    System.out.println(kilometer);
-                    cars.get(i).moveCar(kilometer);
-                    if(cars.get(i).getSpeed() + 10 > 180){
-                        System.out.println("You can not increase the speed more! Stay safe!");
-                    }
-                    else{
-                        cars.get(i).changeSpeed(10);
-                    }
-                    if(cars.get(i).getNumberOfKilometers() == 0 && !ok){
-                        System.out.println("The first car whose kilometers resets for the first time is " + i);
-                        ok = true;
-                    }
-                    System.out.println(cars.get(i).toString());
-                }
-
+        // read the attributes of the car from a file (brand, type and year of manufacture)
+        Random rand = new Random();
+        File file = new File("input.txt");
+        Scanner scanner = new Scanner(file);
+        int numberCars = Integer.parseInt(scanner.nextLine()); // number of cars in the file
+        int i = 0;
+        String line;
+        ArrayList<Car> cars = new ArrayList<>(numberCars); // each element of the array is a new car
+        while(scanner.hasNextLine() && i < numberCars){
+            int fuelType = rand.nextInt(4);
+            boolean passengers = rand.nextBoolean();
+            line = scanner.nextLine();
+            try{
+                StringTokenizer tokens = new StringTokenizer(line, ", ");
+                cars.add(new Car(tokens.nextToken(), tokens.nextToken(),fuelType, passengers, Integer.parseInt(tokens.nextToken())));
+                System.out.println(cars.get(i).toString());
+                i++;
             }
-            System.out.println("The number of diesel cars is " + countDiesel);
-            System.out.println("The number of red cars is " + countDiesel);
-            myReader.close();
-            }catch(FileNotFoundException e){
-                System.out.println("An error occurred. Can not open the file!");
-                e.printStackTrace();
-             }
+            catch(Exception e){
+                System.out.println("An error occurred whilst adding a new car!");
+            }
         }
+
+        System.out.println("\n\nIt's time to travel!");
+        Scanner read = new Scanner(System.in);
+
+        int countDiesel = 0;  // variable to count the number of the diesel cars
+        int countRedCars = 0; // variable to count the number of the red cars
+        boolean reset = false; // boolean to check which car's kilometer resets first
+        int firstReset = -1; // variable to store the index of the first car which mileage resets
+
+        for(i = 0; i < numberCars; i++){
+            System.out.println("\n" + cars.get(i).toString()); // print which car is moving
+            System.out.println("Enter how many times you want to move the car: ");
+            int increase = read.nextInt();
+            boolean increaseSpeed = false; // boolean to check if a car has already reached the speed limit or not
+            for(int j = 0; j  < increase; j++){
+                int kilometers = 1 + rand.nextInt(50); // random generate the distance the car should travel
+                if(cars.get(i).getNumberOfKilometers() + kilometers >= 200 && !reset){
+                    firstReset = i;
+                    reset = true;
+                }
+//                System.out.println("Moving the car " + kilometers + " kilometers.");
+                cars.get(i).moveCar(kilometers);
+
+                if(cars.get(i).getSpeed() + 10 > 180 && !increaseSpeed){ // check speed limit
+                    System.out.println("You reached the speed limit! Stay safe!");
+                    increaseSpeed = true;
+                }
+                else if (cars.get(i).getSpeed() + 10 <= 180 && !increaseSpeed){
+                    // if it hasn't reached the speed limit yet, increase the speed with 10 km / h
+                    cars.get(i).changeSpeed(10);
+                }
+            }
+
+            System.out.println("The " + cars.get(i).getCarBrand() + " " +
+                    cars.get(i).getCarType() + " has traveled " + cars.get(i).getNumberOfKilometers() +
+                    " kilometers and reached the speed of " + cars.get(i).getSpeed() + "km/h.");
+
+            if(cars.get(i).getFuel() == FuelType.valueOf("DIESEL")){
+                countDiesel += 1;
+            }
+
+            if(cars.get(i).getColor() == Color.valueOf("RED")){
+                countRedCars += 1;
+            }
+
+        }
+        System.out.println("\nThe kilometers for the car with the index number " + firstReset + " were reset first.");
+        System.out.println("The number of red cars is " + countRedCars + ".");
+        System.out.println("The number of diesel cars is " + countDiesel + ".");
+    }
 }
 
 
